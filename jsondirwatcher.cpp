6d977b0a6f88b66d3401031436caeb8e5a0df33e
TerehovIM <terehovim@gmail.com>
2021-12-25 02:28:56 +0300

    1

--- jsondirwatcher.cpp
+++ jsondirwatcher.cpp
@@ -7,40 +7,63 @@ JsonDirWatcher::JsonDirWatcher(QString path)
     connect(timer, SIGNAL(timeout()), this, SLOT(checkJsonDir()));
     timer->start(timerPeriod);
 }
+
 void JsonDirWatcher::checkJsonDir()
 {
-    QFileInfoList buffer = lastInfo;
     QFileInfoList newInfo = QDir(jsonFilesPath).entryInfoList(QDir::Files|QDir::NoDotAndDotDot|QDir::NoSymLinks|QDir::Readable, QDir::Time);
     QMutableListIterator<QFileInfo> infoIt(newInfo);
     while(infoIt.hasNext())
     {
         if(infoIt.next().suffix() != "json")
             infoIt.remove();
-        else
-        {
-            for(int n = 0; n < lastInfo.count(); n++)
-            {
-                if(infoIt.value() == lastInfo.at(n))
-                {
-                    lastInfo.removeAt(n);
-                    infoIt.remove();
-                    break;
-                }
-            }
-        }
     }
-    foreach(QFileInfo toAdd, newInfo)
-    {
-        //addToModel(newInfo)
-        buffer.
-    }
-
-    //removeFromMode(lastInfo)
-    //lastInfo = getModelList()
-
     if(newInfo!=lastInfo)
     {
         lastInfo.clear();
         lastInfo = newInfo;
+        readInfoFromFiles(newInfo);
     }
+}
+
+void JsonDirWatcher::setPath(QString path)
+{
+    jsonFilesPath = path;
+}
+
+QList<JsonInfo> JsonDirWatcher::readInfoFromFiles(QFileInfoList infoList)
+{
+    QList<JsonInfo> buffer;
+    foreach(QFileInfo fileInfo, infoList)
+    {
+        QFile jsonFile(fileInfo.absoluteFilePath());
+        if(!jsonFile.open(QIODevice::ReadOnly))
+        {
+            //QMessageBox::warning(this,"",QString("Ошибка чтения файла "+fileInfo.absoluteFilePath()));
+            return buffer;
+        }
+        QByteArray jsonFileData = jsonFile.readAll();
+        jsonFile.close();
+
+        QJsonParseError parseError;
+        QJsonDocument jsonDocument(QJsonDocument::fromJson(jsonFileData,&parseError));
+        if(parseError.error != QJsonParseError::NoError)
+        {
+            //QMessageBox::warning(this,"",QString("Error at"+QString.fromInt(parseError.offset)+" : "+parseError.errorString()));
+            return buffer;
+        }
+
+        QJsonObject jsonObj = jsonDocument.object();
+        QJsonArray jsonArray;
+        jsonArray = jsonObj["list"].toArray();
+        foreach(QJsonValue val, jsonArray)
+        {
+            QJsonObject obj = val.toObject();
+            JsonInfo jsonInf(obj.value(QString::number(0)).toInt(),
+                             obj.value(QString::number(1)).toString(),
+                             obj.value(QString::number(2)).toBool(),
+                             obj.value(QString::number(3)).toDouble());
+            buffer.append(jsonInf);
+        }
+    }
+    return buffer;
 }
--- jsondirwatcher.h
+++ jsondirwatcher.h
@@ -1,22 +1,29 @@
 #ifndef JSONDIRWATCHER_H
 #define JSONDIRWATCHER_H
 
+#include "jsoninfo.h"
 #include <QObject>
 #include <QTimer>
 #include <QFileInfo>
 #include <QFile>
 #include <QDir>
+#include <QMessageBox>
+#include <QJsonParseError>
+#include <QJsonObject>
+#include <QJsonArray>
 
 class JsonDirWatcher : public QObject
 {
     Q_OBJECT
 public:
     JsonDirWatcher(QString path);
+    void setPath(QString path);
 private:
     QTimer *timer;
     int timerPeriod = 100;
     QString jsonFilesPath;
     QFileInfoList lastInfo;
+    QList<JsonInfo> readInfoFromFiles(QFileInfoList infoList);
 private slots:
     void checkJsonDir();
 
