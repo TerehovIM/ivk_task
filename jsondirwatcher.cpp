c6b1228f0891a1a0b5b8e0c2f7d0846cea2e513c
TerehovIM <terehovim@gmail.com>
2021-12-25 01:27:50 +0300

    Trying remake jsonDIrWatcher

--- jsondirwatcher.cpp
+++ jsondirwatcher.cpp
@@ -13,13 +13,35 @@ void JsonDirWatcher::setPath(QString path)
 }
 void JsonDirWatcher::checkJsonDir()
 {
+    QFileInfoList buffer = lastInfo;
     QFileInfoList newInfo = QDir(jsonFilesPath).entryInfoList(QDir::Files|QDir::NoDotAndDotDot|QDir::NoSymLinks|QDir::Readable, QDir::Time);
     QMutableListIterator<QFileInfo> infoIt(newInfo);
     while(infoIt.hasNext())
     {
         if(infoIt.next().suffix() != "json")
             infoIt.remove();
+        else
+        {
+            for(int n = 0; n < lastInfo.count(); n++)
+            {
+                if(infoIt.value() == lastInfo.at(n))
+                {
+                    lastInfo.removeAt(n);
+                    infoIt.remove();
+                    break;
+                }
+            }
+        }
     }
+    foreach(QFileInfo toAdd, newInfo)
+    {
+        //addToModel(newInfo)
+        buffer.
+    }
+
+    //removeFromMode(lastInfo)
+    //lastInfo = getModelList()
+
     if(newInfo!=lastInfo)
     {
         lastInfo.clear();
